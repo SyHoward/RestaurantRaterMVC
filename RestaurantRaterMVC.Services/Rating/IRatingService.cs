d1cd5d20c047eaed05397ac7cdeda7ef4196d26d
Sy Howard <symanthacrh@gmail.com>
2023-12-02 15:12:01 -0500

    RatingIndexView

--- RestaurantRaterMVC.Services/Rating/IRatingService.cs
+++ RestaurantRaterMVC.Services/Rating/IRatingService.cs
@@ -1,6 +1,9 @@
+using RestaurantRaterMVC.Models.Rating;
+
 namespace RestaurantRaterMVC.Services.Rating;
 
 public interface IRatingService
 {
-
+    Task<List<RatingListItem>> GetRatingsAsync();
+    Task<List<RatingListItem>> GetRestaurantRatingsAsync(int restaurantId);
 }
--- RestaurantRaterMVC.Services/Rating/RatingService.cs
+++ RestaurantRaterMVC.Services/Rating/RatingService.cs
@@ -1,12 +1,44 @@
+using Microsoft.EntityFrameworkCore;
 using RestaurantRaterMVC.Data;
+using RestaurantRaterMVC.Models.Rating;
 
 namespace RestaurantRaterMVC.Services.Rating;
 
 public class RatingService : IRatingService
 {
-    private readonly RestaurantDbContext _content;
+    private readonly RestaurantDbContext _context;
     public RatingService(RestaurantDbContext context)
     {
-        _content = context;
+        _context = context;
+    }
+
+    public async Task<List<RatingListItem>> GetRatingsAsync()
+    {
+        var ratings = await _context.Ratings
+            .Include(r => r.Restaurant)
+            .Select(r => new RatingListItem
+            {
+                RestaurantName = r.Restaurant.Name,
+                Score = r.Score
+
+            })
+            .ToListAsync();
+        
+        return ratings;
+    }
+
+    public async Task<List<RatingListItem>> GetRestaurantRatingsAsync(int restaurantId)
+    {
+        var ratings = await _context.Ratings
+            .Include(r => r.Restaurant)
+            .Where(r => r.RestaurantId == restaurantId)
+            .Select(r => new RatingListItem
+            {
+                RestaurantName = r.Restaurant.Name,
+                Score = r.Score
+            })
+            .ToListAsync();
+        
+        return ratings;
     }
 }
